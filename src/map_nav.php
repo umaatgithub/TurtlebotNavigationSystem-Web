f59d88240dcbe8ad429d3fd90ffcad5b0e8d0b40
umaatgithub <umamahesh.research@gmail.com>
2016-04-21 14:19:10 +0200

    Modified map_nav.php for dynamic ip

--- src/map_nav.php
+++ src/map_nav.php
@@ -8,8 +8,7 @@ if(!(isset($_SESSION['user_name']))){
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Navigation</title>
-
+    <title>Map Navigation</title>
 <style>
 td {
     border: 1px solid black;
@@ -21,66 +20,50 @@ td {
 <script type="text/javascript" src="http://cdn.robotwebtools.org/roslibjs/current/roslib.min.js"></script>
 <script type="text/javascript" src="http://cdn.robotwebtools.org/ros2djs/current/ros2d.min.js"></script>
 <script type="text/javascript" src="http://cdn.robotwebtools.org/nav2djs/current/nav2d.min.js"></script>
-<script type="text/javascript">
-  /**
-   * Setup all GUI elements when the page is loaded.
-   */
-  function init() {
-    // Connect to ROS.
-    var ros = new ROSLIB.Ros({
-      url : 'ws://192.168.0.100:9090'
-    });
-
-    // Create the main viewer.
-    var viewer = new ROS2D.Viewer({
-      divID : 'nav',
-      width : 750,
-      height : 800
-    });
-
-    // Setup the nav client.
-    var nav = NAV2D.OccupancyGridClientNav({
-      ros : ros,
-      rootObject : viewer.scene,
-      viewer : viewer,
-      continuous : true,
-      serverName : 'move_base'
-    });
-  }
-</script>
+<script type="text/javascript" src="js/map_nav.js"></script>
 </head>
-<h3 align="right"><?php echo $_SESSION['user_name']; ?>&nbsp;&nbsp;&nbsp;<a href="logout.php">LOGOUT</a>&nbsp;&nbsp;&nbsp;&nbsp;</h3>
-<body onload="init()">
+
+<body onload="loadMap()">
+
+<h3 align="right">
+    <?php echo $_SESSION['user_name']; ?>&nbsp;&nbsp;&nbsp;<a href="logout.php">LOGOUT</a>&nbsp;&nbsp;&nbsp;&nbsp;
+</h3>
+
+<h1 align="center">TURTLEBOT NAVIGATION SYSTEM</h1>
+
 <table style="width:100%; height:100%; text-align: center;">
 
 <tr>
-    <th colspan='3'><h1>TURTLEBOT NAVIGATION SYSTEM</h1></th> 
-</tr>
-
-<tr>
-    <td width='200px' height='25px'><a href="manual_nav.php">Manual Control</a></td> 
-    <td rowspan='4'>
-        <table style="width:100%; height:100%; text-align: center;">
-
-<tr>
-    <td width='500px' height='500px'>
-	<iframe src="http://192.168.0.100:8080/stream_viewer?topic=/camera/rgb/image_color" width= "370" height="340"></iframe>
+    <td  width="250px" height="25px">Host IP address : <? echo $_COOKIE['host_ip']; ?></td> 
+    <td colspan="2" align="left" height="25px">&nbsp;&nbsp;Connection status : 
+	<input id="conn_img" name="conn_img" src="images/disconnected.png" type="image" width="10px" height="10px"/>     
+	<span id="conn_status" name="conn_status"></span>
     </td>
+</tr> 
 
-<td>
-	<div id="nav"></div>
-    </td> 
-  </tr>
-        </table>
-    </td> 
+<tr>
+    <td width="250px" height="25px"><a href="manual_nav.php">Manual Control</a></td> 
+    <td rowspan="4">
+        <table style="width:100%; height:100%; text-align: center;">
+            <tr>
+            <td width='500px' height='500px'>
+		<iframe id="video_stream" src="" width= "370" height="340"></iframe>
+            </td>
+
+            <td>
+		<div id="map"></div>
+            </td> 
+	    </tr>
+	</table>
+    </td>
 </tr>
 
 <tr>
-    <td width='200px' height='25px'><a href="map_nav.php">Map Navigation</a></td> 
+    <td width="250px" height="25px"><a href="map_nav.php">Map Navigation</a></td> 
 </tr>
 
 <tr>
-    <td width='200px' height='25px'><a href="configure.php">Settings</a></td> 
+    <td width="250px" height="25px"><a href="configure.php">Configure Base</a></td> 
 </tr>
 
 <tr>
@@ -89,3 +72,5 @@ td {
 
 </table>
 </body>
+
+</html>
